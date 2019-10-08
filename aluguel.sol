fa68fdc8b4be31af692294d0bf838b4a6ed9913f
rodrigopfernandes <56077942+rodrigopfernandes@users.noreply.github.com>
2019-10-08 20:54:57 -0300

    Atualização do aluguel.sol
    Complementação e modificação da disposição do código.

--- aluguel.sol
+++ aluguel.sol
@@ -1,23 +1,43 @@
-pragma solidity ^0.5.12;
+pragma solidity ^0.5.11;
 
 // Contrato de locação
-contract Aluguel {
+contract Aluguel
+{
     string public locador;
     string public locatario;
     uint256 private valor;
     uint256 constant numeroMaximoLegalDeAlugueisParaMulta = 3;
-    constructor(string memory nomeLocador, string memory nomeLocatario, uint256 valorDoAluguel) public {
+    
+    constructor(string memory nomeLocador, string memory nomeLocatario, uint256 valorDoAluguel) public
+    {
         locador = nomeLocador;
         locatario = nomeLocatario;
         valor = valorDoAluguel;
     }
-    function valorAtualDoAluguel() public view returns (uint256) {
+    
+    function valorAtualDoAluguel() public view returns (uint256) 
+    {
         return valor;
     }
-    function simulaMulta (uint256 mesesRestantes, uint256 totalMesesContrato) public view returns (uint256 valorMulta) {
+    
+    function simulaMulta (uint256 mesesRestantes, uint256 totalMesesContrato) public view returns (uint256 valorMulta)
+    {
         valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
         valorMulta = valorMulta/totalMesesContrato;
         valorMulta = valorMulta*mesesRestantes;
         return valorMulta;
     }
+    
+    function reajustaAluguel(uint256 percentualReajuste) public 
+    {
+        uint256 valorDoAcrescimo = 0;
+        valorDoAcrescimo = ((valor*percentualReajuste)/100);
+        valor = valor + valorDoAcrescimo;
+    }
+    
+    function aditamentoValorAluguel (uint256 valorCerto) public
+    {
+        valor = valorCerto;
+    }
+    
 }   
